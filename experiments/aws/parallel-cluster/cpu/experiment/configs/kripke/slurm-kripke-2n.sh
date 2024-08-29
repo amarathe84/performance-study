fce48c7da04c61ee161314df28920246662b11e3
AWS ParallelCluster user <ubuntu@ip-10-0-15-142.us-east-2.compute.internal>
2024-08-29 22:30:03 +0000

    Kripke updates

--- experiments/aws/parallel-cluster/cpu/experiment/configs/kripke/slurm-kripke-2n.sh
+++ experiments/aws/parallel-cluster/cpu/experiment/configs/kripke/slurm-kripke-2n.sh
@@ -9,5 +9,15 @@ export OMP_NUM_THREADS=1
 echo "Start time:" $( date +%s )
 /usr/bin/time -p mpirun -N 2 --map-by ppr:96:node \
 	                    /shared/bin/singularity exec /shared/containers/metric-kripke-cpu_libfabric-zen4.sif kripke \
-			    --layout GDZ --dset 8 --zones 96,96,96 --gset 16 --groups 64 --niter 10 --legendre 9 --quad 8 --procs 4,6,8
+			    --layout GDZ --dset 8 --zones 96,96,96 --gset 16 --groups 64 --niter 10 --legendre 2 --quad 8 --procs 4,6,8
 echo "End time:" $( date +%s )
+
+#full-scale input OOMs (expected behavior for the problem size)
+export OMP_NUM_THREADS=1
+echo "Start time:" $( date +%s )
+/usr/bin/time -p mpirun -N 2 --map-by ppr:96:node \
+	                    /shared/bin/singularity exec /shared/containers/metric-kripke-cpu_libfabric-zen4.sif kripke \
+			    --layout DGZ --dset 16 --zones 448,280,256 --gset 16 --groups 16 --niter 10 --legendre 2 --quad 8 --procs 4,6,8
+echo "End time:" $( date +%s )
+
+
--- experiments/aws/parallel-cluster/cpu/experiment/configs/kripke/slurm-kripke-32n.sh
+++ experiments/aws/parallel-cluster/cpu/experiment/configs/kripke/slurm-kripke-32n.sh
@@ -9,5 +9,5 @@ export OMP_NUM_THREADS=1
 echo "Start time:" $( date +%s )
 /usr/bin/time -p mpirun -N 32 --map-by ppr:96:node \
 	                    /shared/bin/singularity exec /shared/containers/metric-kripke-cpu_libfabric-zen4.sif kripke \
-			    --layout DGZ --dset 16 --zones 448,168,256 --gset 16 --groups 16 --niter 500 --legendre 2 --quad 16 --procs 16,12,16
+			    --layout DGZ --dset 16 --zones 448,168,256 --gset 16 --groups 16 --niter 400 --legendre 2 --quad 16 --procs 16,12,16
 echo "End time:" $( date +%s )
